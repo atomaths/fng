bb4da75a05472b05ffeeae9e63765789e1e246f0
Jongmin Kim <atomaths@gmail.com>
2014-01-14 17:22:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Simple find and grep command line tool
+
+The reuslt of fng is easy to see output.
+
+### Installation
+
+```bash
+$ go get github.com/atomaths/fng
+```
+
+### Usage
+
+```bash
+$ fng *.go string_to_find
+$ fng *.go *.c "string to find"
+```
+
+### API reference
+[godoc.org/github.com/atomaths/fng](http://godoc.org/github.com/atomaths/fng)
---
+++ main.go
@@ -0,0 +1,44 @@
+// Copyright 2014 Jongmin Kim. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/scanner"
+	"os"
+)
+
+var (
+	exitCode = 0
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: fng [file name patterns...] [match string]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func report(err error) {
+	scanner.PrintError(os.Stderr, err)
+	exitCode = 2
+}
+
+func gofmtMain() {
+	flag.Usage = usage
+	flag.Parse()
+
+	for i := 0; i < flag.NArg(); i++ {
+		fmt.Println(flag.Arg(i))
+	}
+}
+
+func main() {
+	// call gofmtMain in a separate function
+	// so that it can use defer and have them
+	// run before the exit.
+	gofmtMain()
+	os.Exit(exitCode)
+}
